a2e3ae4ea53f468450918c648fdcf7610008981f
Trijesh Kondapuram <144296232+trijesh61@users.noreply.github.com>
2026-07-20 22:25:47 +0530

    Updated

--- us_visa/exception/__init__.py
+++ us_visa/exception/__init__.py
@@ -1,22 +1,17 @@
 import os
 import sys
 
-
-def error_message_detail(error, error_detail: sys):
+def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
-
     file_name = exc_tb.tb_frame.f_code.co_filename
-    line_number = exc_tb.tb_lineno
-
     error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, line_number, str(error)
+        file_name, exc_tb.tb_lineno, str(error)
     )
 
     return error_message
 
-
 class USvisaException(Exception):
-    def __init__(self, error_message, error_detail: sys):
+    def __init__(self, error_message, error_detail):
         """
         :param error_message: error message in string format
         """
